27f843db2d126fdec1915a5815309d4e0300b2e2
Kattya Torrez <44486786+kattya17@users.noreply.github.com>
2024-01-14 19:25:27 -0400

    all more

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './app/**/*.html',    // Ruta a los archivos HTML en la carpeta 'app' (ajusta según sea necesario)
+    './app/**/*.js',  
+    './app/**/*.tsx',     // Ruta a los archivos JavaScript en la carpeta 'app' (ajusta según sea necesario)
+    // Agrega otras rutas según sea necesario
+  ],
+  // ...
+};
