d32019fa1d99f3f28932e6a91253190a4c1c7b4e
Kattya Torrez <44486786+kattya17@users.noreply.github.com>
2024-01-14 23:40:15 -0400

    Add files via upload

---
+++ fonts.ts
@@ -0,0 +1,7 @@
+import { Inter as MyInter, Lusitana } from 'next/font/google';
+
+export const inter = MyInter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
